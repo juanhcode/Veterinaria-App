972b24ead176ed8b9910d093ce127bbba6fdca28
haroldiaz <jean.alviz@correounivalle.edu.co>
2022-04-15 11:16:12 -0500

    cambio de nombre de rama - copia repo master

---
+++ apps/veterinario/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+# Create your views here.
+class ListViewVeterinario(TemplateView):
+    template_name = 'veterinaria/home.html'
