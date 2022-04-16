8b6a418e96ea393738654150fec4be4744834e59
Juan Hoyos <juanhoyos1347@gmail.com>
2022-04-15 22:29:46 -0500

    url de historial

--- apps/veterinario/urls.py
+++ apps/veterinario/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'veterinaria_app'
 urlpatterns = [
     path('home/',views.ListViewVeterinario.as_view(),name='home'),
-    path('historial/',views.ListViewVeterinario.as_view(),name='historial'),
+    path('historial/',views.ListViewHistorial.as_view(),name='historial'),
     path('ventas/',views.ListViewVentas.as_view(),name='ventas'),
 
 ]
