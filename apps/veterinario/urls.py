98e9d1312b73dcaa8f8b0666c0d6989c6a162b93
GregoryG8 <gregory.gonzalez@correounivalle.edu.co>
2022-04-22 12:06:17 -0500

    Añadiendo visual de historial

--- apps/veterinario/urls.py
+++ apps/veterinario/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'veterinaria_app'
 urlpatterns = [
     path('home/',views.ListViewVeterinario.as_view(),name='home'),
-    path('historial/',views.ListViewVeterinario.as_view(),name='historial'),
     path('historial/',views.ListViewHistorial.as_view(),name='historial'),
     path('login/',views.ListViewLogin.as_view(),name='login'),
 
