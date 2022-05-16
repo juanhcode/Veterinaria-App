58d1623ae8d9792ca8232630bde6b46467a226d5
SpecTr03 <sebastian.cifuentes0808@gmail.com>
2022-05-15 22:45:46 -0500

    .

---
+++ apps/users/models.py
@@ -0,0 +1,48 @@
+from pyexpat import model
+from django.db import models
+
+from .managers import UserManagerVeterinaria
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    TYPE_USER =(
+        ('1','Veterinario'),
+        ('2','Vendedor'),
+        ('3','Administrador'),
+    )
+
+    GENERO =(
+        ('M','Masculino'),
+        ('F','Femenino'),
+        ('O','Otro'),
+    )
+    
+    nombre = models.CharField('Nombre', max_length=20,null=True)
+    apellidos = models.CharField('Apellidos', max_length=50,null=True)
+    cedula = models.CharField('Cedula', max_length=12, unique=True)
+    fecha_ingreso = models.DateField('Fecha de Ingreso', null=True)
+    edad = models.PositiveIntegerField('Edad',null=True)
+    sexo = models.CharField('Sexo', choices=GENERO,max_length=1,null=True)
+    correo = models.EmailField('Correo Electronico',null=True)
+    telofono = models.CharField('Telofono', max_length=10,null=True)
+    tipo_usuario = models.CharField('Tipo de Usuario', choices=TYPE_USER, max_length=1,null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    objects = UserManagerVeterinaria()
+
+    USERNAME_FIELD = 'cedula'
+
+    REQUIRED_FIELDS = [
+        'correo'
+    ]
+   
+
+    def get_full_name(self):
+        return self.nombre + ' ' + self.apellidos
+    
+
