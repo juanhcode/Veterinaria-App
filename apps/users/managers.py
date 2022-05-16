1cbf209483effa62ba798703610a73e2f7ae409d
SpecTr03 <sebastian.cifuentes0808@gmail.com>
2022-05-15 22:36:31 -0500

    modelos de usuario version alpha

---
+++ apps/users/managers.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from django.contrib.auth.models import BaseUserManager
+
+class UserManagerVeterinaria(BaseUserManager, models.Manager):
+
+    #aqui es donde se pasan los parametros con los que se creara un superusuario o un superusuario
+    ''' esta funcion decide como crear los superusuarios segun los parametros que le enviemos por ejemplo:
+    los usuarios normales tienen en su return dos False, es decir que no seran staff ni superusuarios  '''
+    def _create_user(self, cedula, correo, password, is_staff, is_superuser,is_active, **extra_fields):
+        user = self.model(
+            cedula=cedula,
+            correo = correo,
+            is_staff=is_staff,
+            is_superuser= is_superuser,
+            is_active = is_active,
+            **extra_fields
+        )
+        #encriptando el password
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, cedula, correo, password=None, **extra_fields):
+        return self._create_user(cedula, correo, password, False, False, True, **extra_fields)
+
+    def create_superuser(self, cedula, correo, password=None, **extra_fields):
+        return self._create_user(cedula, correo, password, True, True,True, **extra_fields)
