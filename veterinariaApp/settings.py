4f041c875f9c2abd8bb6e54e3fee02822c2e656d
SpecTr03 <sebastian.cifuentes0808@gmail.com>
2022-05-23 22:06:15 -0500

    verificando push de master por harold

--- veterinariaApp/settings.py
+++ veterinariaApp/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'veterinariaApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -104,7 +104,8 @@ DATABASES = {
         'HOST' : 'localhost',
         'DATABASE_PORT' : '5432',
     }
-}"""
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
