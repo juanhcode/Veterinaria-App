9312ee4e3108b7abfbbc74e3326ab53592e1b31a
GregoryG8 <gregory.gonzalez@correounivalle.edu.co>
2022-06-06 21:48:20 -0500

    Ajuste en formulario-clienteResponsable
    Se organizo con nueva imagen, posision mas confortable, terminando el boton de "Gestionar Cliente/Responsable"

--- apps/veterinario/urls.py
+++ apps/veterinario/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 app_name = 'veterinaria_app'
 urlpatterns = [
-    path('',views.ListViewVeterinario.as_view(),name='home'),
+    path('home/',views.ListViewVeterinario.as_view(),name='home'),
     path('historial/',views.ListViewHistorial.as_view(),name='historial'),
+    path('login/',views.ListViewLogin.as_view(),name='login'),
+    path('inicio-historial/',views.ListViewInicioHistorial.as_view(),name='inicio-historial'),
+    path('cliente-responsable/',views.ListViewClienteResponsable.as_view(),name='cliente-responsable'),
+    path('formulario-cliente/',views.ListViewFormularioClienteResponsable.as_view(),name='formulario-cliente'),
 
 ]
--- apps/veterinario/views.py
+++ apps/veterinario/views.py
@@ -7,6 +7,17 @@ from django.views.generic import TemplateView
 class ListViewVeterinario(TemplateView):
     template_name = 'veterinaria/home.html'
 
+class ListViewLogin(TemplateView):
+    template_name = 'login/Inicio.html'
+
 class ListViewHistorial(TemplateView):
     template_name = 'historial/historial.html'
 
+class ListViewInicioHistorial(TemplateView):
+    template_name = 'historial/inicio-historial.html'
+
+class ListViewClienteResponsable(TemplateView):
+    template_name = 'historial/cliente-responsable.html'
+
+class ListViewFormularioClienteResponsable(TemplateView):
+    template_name = 'historial/formulario-clientes.html'
